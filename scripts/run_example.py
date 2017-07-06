b338e28bedf93601db25267b62213637d0c9da52
Lisa Zorn <lzorn@mtc.ca.gov>
2017-07-05 18:34:30 -0700

    Fix run_example to work with multiprocess config

--- scripts/run_example.py
+++ scripts/run_example.py
@@ -2,19 +2,20 @@ import os
 
 from fasttrips import Run
 
-EXAMPLES_DIR   = os.path.join(os.path.dirname(os.getcwd()),"Examples","test_scenario")
+# note: this MUST be in __main__ or the run will error when trying to fork subprocesses
+if __name__ == "__main__":
 
-Run.run_fasttrips(
-    input_network_dir    = os.path.join(EXAMPLES_DIR,"network"),
-    input_demand_dir = os.path.join(EXAMPLES_DIR,"demand_reg"),
-    run_config       = os.path.join(EXAMPLES_DIR,"demand_reg","config_ft.txt"),
-    input_weights    = os.path.join(EXAMPLES_DIR,"demand_reg","pathweight_ft.txt"),
-    output_dir       = os.path.join(EXAMPLES_DIR,"output"),
-    output_folder    = "example",
-    pathfinding_type = "stochastic",
-    overlap_variable = "count",
-    overlap_split_transit = True,
-    iters            = 1,
-    dispersion       = 0.50)
+    EXAMPLES_DIR   = os.path.join(os.getcwd(),"fasttrips","Examples","test_scenario")
 
-    
+    Run.run_fasttrips(
+        input_network_dir     = os.path.join(EXAMPLES_DIR,"network"),
+        input_demand_dir      = os.path.join(EXAMPLES_DIR,"demand_reg"),
+        run_config            = os.path.join(EXAMPLES_DIR,"demand_reg","config_ft.txt"),
+        input_weights         = os.path.join(EXAMPLES_DIR,"demand_reg","pathweight_ft.txt"),
+        output_dir            = os.path.join(EXAMPLES_DIR,"output"),
+        output_folder         = "example",
+        pathfinding_type      = "stochastic",
+        overlap_variable      = "count",
+        overlap_split_transit = True,
+        iters                 = 1,
+        dispersion            = 0.50)
